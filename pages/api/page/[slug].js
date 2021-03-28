62a10547511140a34c406a45b07c03d087cfbaeb
Łukasz Masłowski <l.maslowski91@gmail.com>
2021-03-28 11:36:42 +0200

    getting data from wpgraphql

---
+++ pages/api/page/[slug].js
@@ -0,0 +1,30 @@
+ export default async (req, res) => {
+     const { query: { slug } } = req;
+
+     const QUERY_SINGLE_POST = `
+            query SinglePost($id: ID!) {
+                page(id: $id, idType: URI) {
+                title
+                content
+        } 
+    }
+     `;
+
+     const data = await fetch( process.env.
+       WORDPRESS_LOCAL_API_URL,  {
+            method: 'POST',
+           headers: { 'Content-Type': 'application/json' },
+           body: JSON.stringify({ 
+                query: QUERY_SINGLE_POST,
+            variables: {
+                id:slug
+            } 
+        })
+       });
+       const json = await data.json();
+
+        console.log(data);
+
+        res.json(data);
+        // return data;
+    }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,16 @@ import Head from 'next/head'
 
 
 export default function Home() {
+  const data = fetch ( 'http://headlessnext.local/graphql', {
+headers: { 'Content-Type': 'application/json' },
+method: 'POST',
+body: JSON.stringify({ 'test': 'value'})
+  });
+  console.log(data);
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App</title> 
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
